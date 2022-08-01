11188421a19899b4e78db54fe4ed7f2db68e21da
Vladislav Kalesnik <vlad.kolesnik.99@list.ru>
2022-08-01 19:52:49 +0300

    update files

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ const container = document.querySelector('.slider__container');
 const track = document.querySelector('.slider__track');
 const items = document.querySelectorAll('.slider__item');
 const itemsCount = items.length;
-
 const btnPrev = document.querySelector('.btn-prev');
 const btnNext = document.querySelector('.btn-next');
-
+const sliderDots = document.querySelectorAll('.dot');
 const itemWidth = container.clientWidth / slidesToShow;
 const movePosition = slidesToScroll * itemWidth;
-
 items.forEach((item) => {
   item.style.minWidth = `${itemWidth}px`;
 });
@@ -30,11 +28,24 @@ btnPrev.addEventListener('click', () => {
 
   setPosition();
   checkBtns();
+  setDots();
 });
 
 btnNext.addEventListener('click', () => {
   nextSlide();
+  setDots();
 });
+
+for (let i = 0; i < sliderDots.length; i++) {
+  sliderDots[i].addEventListener('click', (element) => {
+    for (let i = 0; i < sliderDots.length; i++) {
+      sliderDots[i].className = sliderDots[i].className.replace(' active', '');
+    }
+    sliderDots[i].className += ' active';
+    position = -i * itemWidth;
+    setPosition();
+  });
+}
 
 const setPosition = () => {
   track.style.transform = `translateX(${position}px)`;
@@ -59,6 +70,14 @@ const nextSlide = () => {
   checkBtns();
 };
 
+const setDots = () => {
+  for (let i = 0; i < sliderDots.length; i++) {
+    sliderDots[i].className = sliderDots[i].className.replace(' active', '');
+  }
+  sliderDots[Math.abs(position) / itemWidth].className += ' active';
+};
+
 setInterval(function () {
   nextSlide();
+  setDots();
 }, 7000);
